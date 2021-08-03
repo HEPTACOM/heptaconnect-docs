fc6a6f368c64bb31e3e524f80cde354b88f9b2c8
Joshua Behrens <behrens@heptacom.de>
2021-08-03 14:32:28 +0200

    Introduce color coding
    CONNECT-99

---
+++ generators/inject_role_metadata.py
@@ -0,0 +1,31 @@
+import mkdocs_gen_files
+from os import walk
+from os import listdir
+from os.path import isfile, join
+import yaml
+from yaml.loader import SafeLoader
+
+docPath = 'docs'
+onlyfiles = [f for f in listdir(docPath) if isfile(join(docPath, f))]
+
+for root, _, files in walk(docPath):
+    for item in files:
+        if item.endswith(".md"):
+            fullPath = join(root, item)[len(docPath)+1:]
+
+            with mkdocs_gen_files.open(fullPath, "r") as alteredFile:
+                oldContent = alteredFile.read()
+
+            preface = dict()
+            newContentParts = oldContent.split('---', 3)
+
+            if len(newContentParts) == 3:
+                preface = list(yaml.load_all(newContentParts[1], Loader=SafeLoader))[0]
+            else:
+                newContentParts = ['','', newContentParts[0]]
+
+            preface['role'] = fullPath.split('/')[1 if fullPath.startswith('guides/') else 0]
+            newContentParts[1] = yaml.dump(preface)
+
+            with mkdocs_gen_files.open(fullPath, "w") as alteredFile:
+                print('---\n'.join(newContentParts), file=alteredFile)
