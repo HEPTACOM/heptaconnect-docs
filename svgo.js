d969d30ad96a7c32004971c5877c4db156902c06
Joshua Behrens <behrens@heptacom.de>
2021-08-08 05:31:55 +0200

    Minify svg files
    CONNECT-99

---
+++ svgo.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const { optimize } = require('svgo');
+
+const list_files = function(dirPath, result = undefined) {
+    result = result || [];
+
+    if (!fs.existsSync(dirPath)) {
+        return result;
+    }
+
+    fs.readdirSync(dirPath).forEach(function(file) {
+        const realpath = path.join(dirPath, path.sep, file);
+
+        if (fs.statSync(realpath).isDirectory() && !file.startsWith('.')) {
+            result = list_files(realpath, result);
+        } else {
+            result.push(realpath);
+        }
+    });
+
+    return result;
+}
+
+for (const file of list_files('site/assets/uml').filter(a => a.endsWith('.svg'))) {
+    const content = fs.readFileSync(file, { encoding: 'utf8', flag: 'r' });
+    const optimized = optimize(content, {
+        path: file
+    });
+    fs.writeFileSync(file, optimized.data);
+}
