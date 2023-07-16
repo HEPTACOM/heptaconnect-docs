5490898e87c878a8c9d039123d2578c2a18a19f4
Joshua Behrens <behrens@heptacom.de>
2023-07-16 21:16:43 +0200

    Add javascript to devenv

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 
 {
+  # Javascript for pre- and postcompilation steps
+  languages.javascript.enable = true;
+  languages.javascript.package = pkgs.nodejs-18_x;
+
+  # Python for mkdocs
   languages.python.enable = true;
   languages.python.version = "3.11";
   languages.python.venv.enable = true;
